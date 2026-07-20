dd594f87782f7460d6bda51d4b6ee24378d92288
Alcino <alcinobere.08@gmail.com>
2026-07-20 19:16:36 +0900

    Primeiro commit

---
+++ android/app/src/main/kotlin/com/example/dalan_ba_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dalan_ba_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  int rota = 1;
+
+  while (rota <= 5) {
+    print("Verifika Rota $rota ..");
+    print("rota hotu verifikadu");
+    rota++;
+  }
+}
