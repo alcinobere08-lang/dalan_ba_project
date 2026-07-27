ee8fb92eb46fb3e94839b044d299575d4e9bb916
Alcino <alcinobere.08@gmail.com>
2026-07-27 17:09:07 +0900

    dezafiuextra oop

--- lib/dezafiuExtra.dart
+++ lib/dezafiuExtra.dart
@@ -1,4 +1,21 @@
+class Mikrolet {
+  int numeru = 0;
+  String Rota = "";
+  String Motorista = "";
+
+  void mostraInformasaun() {
+    print("Numeru: $numeru");
+    print("Rota: $Rota");
+    print("Motorista: $Motorista");
+  }
+}
+
 void main() {
-  String? destinu;
-  print("Destinu:${destinu ?? "seidauk hili"}");
+  Mikrolet m1 = Mikrolet();
+
+  m1.numeru = 12;
+  m1.Rota = "comoro";
+  m1.Motorista = "Joao";
+
+  m1.mostraInformasaun();
 }
