6bd9fc8922aad0333802e1095031c44113699c15
Alcino <alcinobere.08@gmail.com>
2026-07-28 15:57:25 +0900

    aprende11

---
+++ lib/aprende11.dart
@@ -0,0 +1,20 @@
+//null safety
+
+// void main() {
+//   String? naran;
+//   print(naran);
+// }
+
+// null check
+
+// void main() {
+//   String? naran = "alcino";
+//   print(naran!);
+// }
+
+//null+operator(??)
+
+void main() {
+  String? naran;
+  print(naran ?? "deskonesidu");
+}
